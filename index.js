22a12bb25ebe1bf57ce82cfce550729ca37963a9
tvpeter <withtvpeter@gmail.com>
2019-03-14 13:09:19 +0100

    add partials for header and footer

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.set('views', './src/views/');
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public/')));
 
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res)=> {
     res.render('index');
+});
+app.get('/about', (req, res)=>{
+    res.render('about');
 });
 
 app.listen(port, ()=>{ console.log(`Listening on port ${port}`)})
