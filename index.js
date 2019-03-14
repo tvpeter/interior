85d697fd6f15aa5176ce852cc3b92becb5014d97
tvpeter <withtvpeter@gmail.com>
2019-03-14 15:28:10 +0100

    add contact, services, products, details views

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const app = express();
 app.set('views', './src/views/');
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public/')));
-
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res)=> {
@@ -15,6 +14,19 @@ app.get('/', (req, res)=> {
 app.get('/about', (req, res)=>{
     res.render('about');
 });
+app.get('/services', (req, res)=> {
+    res.render('services');
+});
+app.get('/contact', (req, res)=> {
+    res.render('contact');
+});
+app.get('/products', (req, res)=> {
+    res.render('products');
+});
+app.get('/products/details', (req, res)=>{
+    res.render('details');
+});
+
 
 app.listen(port, ()=>{ console.log(`Listening on port ${port}`)})
 
