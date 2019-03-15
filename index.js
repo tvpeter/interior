8b1cf2db3c7bdaf3c6cfe92b8eba774c3ef1970b
tvpeter <withtvpeter@gmail.com>
2019-03-15 04:17:41 +0100

    add dynamic nav for links

--- index.js
+++ index.js
@@ -11,35 +11,47 @@ app.use('/js', express.static(path.join(__dirname, 'public/js/')));
 app.use('/images', express.static(path.join(__dirname, 'public/images/')));
 const port = process.env.PORT || 3000;
 
+const nav = [ 
+    { link : '/', title : 'Home'},
+    { link : '/about', title: 'About Us'},
+    { link : '/services', title: 'Services'},
+    { link : '/products', title: 'Products'},
+    { link : '/contact', title: 'Contact'},
+]
 app.get('/', (req, res)=> {
-    res.render('index', {title: "FMG Furniture | Homepage"});
+    res.render('index', {nav, title: "FMG Furniture | Homepage"});
 });
 app.get('/about', (req, res)=>{
     res.render('about', {
+        nav,
         title: "FMG Furniture | About Us",
         header: "About Us"
     });
 });
 app.get('/services', (req, res)=> {
     res.render('services', {
+        nav,
         title : "FMG Furniture | Services",
         header: "Our Services"
     });
 });
 app.get('/contact', (req, res)=> {
     res.render('contact', {
+        nav,
         title : "FMG Furniture | Contact",
         header: "Contact Us"
     });
 });
 app.get('/products', (req, res)=> {
     res.render('products', {
+        nav,
         title : "FMG Furniture | Products",
         header: "Our Products"
     });
 });
 app.get('/products/details', (req, res)=>{
     res.render('details', {
+        nav,
         title : "FMG Furniture | Products",
         header: "Product Details"
     });
