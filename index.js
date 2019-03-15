be162b9523b60da296c6287019e71fdbfd9c293d
tvpeter <withtvpeter@gmail.com>
2019-03-15 03:37:20 +0100

    add dynamic page title

--- index.js
+++ index.js
@@ -6,25 +6,43 @@ const app = express();
 app.set('views', './src/views/');
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public/')));
+app.use('/css', express.static(path.join(__dirname, 'public/css/')));
+app.use('/js', express.static(path.join(__dirname, 'public/js/')));
+app.use('/images', express.static(path.join(__dirname, 'public/images/')));
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res)=> {
-    res.render('index');
+    res.render('index', {title: "FMG Furniture | Homepage"});
 });
 app.get('/about', (req, res)=>{
-    res.render('about');
+    res.render('about', {
+        title: "FMG Furniture | About Us",
+        header: "About Us"
+    });
 });
 app.get('/services', (req, res)=> {
-    res.render('services');
+    res.render('services', {
+        title : "FMG Furniture | Services",
+        header: "Our Services"
+    });
 });
 app.get('/contact', (req, res)=> {
-    res.render('contact');
+    res.render('contact', {
+        title : "FMG Furniture | Contact",
+        header: "Contact Us"
+    });
 });
 app.get('/products', (req, res)=> {
-    res.render('products');
+    res.render('products', {
+        title : "FMG Furniture | Products",
+        header: "Our Products"
+    });
 });
 app.get('/products/details', (req, res)=>{
-    res.render('details');
+    res.render('details', {
+        title : "FMG Furniture | Products",
+        header: "Product Details"
+    });
 });
 
 
---
+++ tests/unit/header.test.js
@@ -0,0 +1,9 @@
+const header = require('../../src/views/partials/header.ejs');
+
+describe("Header section for all pages", ()=>{
+
+    it('should contain contact email', ()=>{
+        const contactMail = header
+    });
+
+});
