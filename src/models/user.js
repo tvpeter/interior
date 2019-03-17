56038fa24a33e1ede26f206631c575370f1531a7
tvpeter <withtvpeter@gmail.com>
2019-03-17 05:00:23 +0100

    add joi validation for user schema object

--- src/models/user.js
+++ src/models/user.js
@@ -1,14 +1,23 @@
 const mongoose = require('mongoose');
+const Joi = require('joi');
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true},
     email: { type: String, required: true},
-    phone: {type: Number, required: true},
+    phone: {type: String, required: true},
     password:{type: String, required: true}
 });
 
 const User = mongoose.model('User', userSchema);
 
-
+function validateUser(user){
+    const schema = {
+        name: Joi.string().min(4).max(45).required(),
+        email:Joi.string().email({minDomainAtoms:2}).required(),
+        phone:Joi.string().min(11).max(14).required(),
+        password:Joi.string().min(5).max(26).required()
+    }
+}
 
 exports.User = User;
+exports.validate = validateUser;
