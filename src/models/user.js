c633ed8471517c7ce8a724901bd2d6c9cee06213
tvpeter <withtvpeter@gmail.com>
2019-03-17 04:26:49 +0100

    design user registration form

--- src/models/user.js
+++ src/models/user.js
@@ -2,10 +2,9 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true},
-    username: { type: String, required: true},
-    password:{type: String, required: true},
     email: { type: String, required: true},
-    phone: {type: Number, required: true}
+    phone: {type: Number, required: true},
+    password:{type: String, required: true}
 });
 
 const User = mongoose.model('User', userSchema);
