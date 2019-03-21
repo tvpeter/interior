97003dbe8eb9bf29babf257663603f5d6c791e74
tvpeter <withtvpeter@gmail.com>
2019-03-21 13:59:11 +0100

    add post for service create route

--- src/routes/services.js
+++ src/routes/services.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const {Service, validate} = require('../models/services');
 
 let pageDetails = {
     current : "Services",
@@ -8,12 +9,51 @@ let pageDetails = {
 }
 
 function servicesRouter(nav){
-return router.get('/', (req, res)=> {
+ const getServices = router.get('/', (req, res)=> {
     res.render('services', {
         nav,
         pageDetails
     });
 });
+
+const getServiceForm = router.get('/create', (req, res) => {
+    res.render('services/create', {
+        nav, pageDetails
+    });
+});
+
+const createService = router.post('/create', async (req, res) => {
+    //validate the post that it contains all params
+     
+
+    const { error } = validate(req.body);
+    if(error) {
+        pageDetails.error = error.details[0].message;
+        return res.status(400).render('services/create', {nav, pageDetails});
+    }
+
+    //check that the req is not in the db
+    const service = await Service.findOne({'title':req.body.title});
+    if(service) {
+        pageDetails.error = "Service already created";
+        return res.status(409).render('services/create', {nav, pageDetails});
+    }
+
+    //create a new service
+    const newService = new Service({
+        title: req.body.title,
+        description: req.body.description
+    });
+    try {
+        await newService.save();
+        return res.status(200).render('services/create', {nav, pageDetails});
+    } catch (error) {
+        pageDetails.error = error;
+        return res.status(400).render('services/create', {nav, pageDetails});
+    }
+});
+
+return [getServices, getServiceForm, createService];
 }
 
 module.exports = servicesRouter;
