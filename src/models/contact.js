b8610da0f52e5c0a2564e062972d5149e8f8eec2
tvpeter <withtvpeter@gmail.com>
2019-03-22 08:17:51 +0100

    add contact model Joi validation

--- src/models/contact.js
+++ src/models/contact.js
@@ -3,13 +3,22 @@ const Joi = require('joi');
 
 const contactSchema = new mongoose.Schema({
     address:  { type: String, minlength: 10, required:true},
-    phone: {type: Array, maxlength: 16, minlength: 11, required: true, trim: true},
+    phone: {type: Array, maxlength: 15, minlength: 11, required: true, trim: true},
     email: { type: String, required: true, trim: true}
 });
 
 const Contact = mongoose.model('Contact', contactSchema);
 
 
-// function validateContact (contact) {
+function validateContact (contact) {
 
-// }
+    const schema = {
+        address: Joi.string().min(10).required(),
+        phone: Joi.number().min(11).max(15).required(),
+        email: Joi.string().email({minDomainAtoms: 2}).required()
+    }
+    return Joi.validate(contact, schema);
+}
+
+exports.Contact = Contact;
+exports.validate = validateContact;
