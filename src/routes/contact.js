ccd8f39d32a284832b7d3172a7b33a27d0833204
tvpeter <withtvpeter@gmail.com>
2019-03-22 08:55:41 +0100

    add post route for contact

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const {Contact, validate} = require('../models/contact');
 
 let pageDetails = {
     current : "Contact",
@@ -9,12 +10,56 @@ let pageDetails = {
 
 
 function contactRouter(nav){
-    return router.get('/', (req, res)=> {
+
+    const contactIndex = router.get('/', (req, res)=> {
         res.render('contact', {
             nav,
             pageDetails
         });
     });
+
+    const createContact = router.post('/', (req, res)=>{
+        // validate that the details are complete
+        const {error} = validate(req.body)
+        if(error) {
+            pageDetails.error = error.details[0].message;
+            return res.status(400).render('contact/create', {
+                nav, pageDetails
+            });
+        }
+
+        //check that it's not in the db
+        const contact = await Contact.find({'address': req.body.address});
+        const address = await Contact.find({'phone': req.body.phone});
+        const email = await Contact.find({'email': req.body.email});
+
+        if(contact || address || email) {
+            pageDetails.error = "contact already created";
+            return res.status(400).render('/contact/create', {
+                nav, pageDetails
+            })
+        }
+        
+        // create and save the contact
+        const newContact = new Contact ({
+            address: req.body.address,
+            phone: req.body.phone,
+            email: req.body.email
+        })
+
+        try {
+            await newContact.save();
+            return res.status(200).render('/contact/create', {
+                nav, pageDetails
+            })
+        } catch (error) {
+            pageDetails.error = error;
+            return res.status(500).render('/contact/create', { nav, pageDetails});
+        }
+
+    });
+
+    return [ contactIndex];
 }
 
 module.exports = contactRouter;
