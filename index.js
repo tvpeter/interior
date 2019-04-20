5b34b66d412c5a98e4fd718095d7656aeb982a4e
tvpeter <withtvpeter@gmail.com>
2019-04-21 00:08:35 +0100

    add category routes

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const methodOverride = require("method-override");
 const path = require("path");
 const app = express();
 
+// override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
 app.set("views", "./src/views/");
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public/")));
@@ -22,12 +25,6 @@ const nav = [
   { link: "/products", title: "Products" },
   { link: "/contact", title: "Contact" }
 ];
-let pageDetails = {
-  current: "",
-  title: "",
-  header: "",
-  error: ""
-};
 
 const users = require("./src/routes/user")(nav);
 const about = require("./src/routes/about")(nav);
@@ -35,12 +32,14 @@ const services = require("./src/routes/services")(nav);
 const contact = require("./src/routes/contact")(nav);
 const products = require("./src/routes/products")(nav);
 const index = require("./src/routes/index")(nav);
+const category = require("./src/routes/category")(nav);
 
 app.use("/users", users);
 app.use("/about", about);
 app.use("/services", services);
 app.use("/contact", contact);
 app.use("/products", products);
+app.use("/category", category);
 app.use("/", index);
 
 const server = app.listen(port, () => {
