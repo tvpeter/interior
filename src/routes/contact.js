3b82fc315ac61494f7b3c4e6c8a7760218c55f05
tvpeter <withtvpeter@gmail.com>
2019-05-05 17:55:54 +0100

    added mail from forms by customers

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -3,19 +3,23 @@ const router = express.Router();
 const { Contact, validate } = require("../models/contact");
 const headers = require("../../middlewares/headers");
 const auth = require("../../middlewares/auth");
-
-let pageDetails = {
-  current: "Contact",
-  title: "FMG Furniture | Contact",
-  header: "Contact Us"
-};
+const nodemailer = require("nodemailer");
+const Joi = require("joi");
+const { Category } = require("../models/category");
 
 function contactRouter(nav) {
   const contactIndex = router.get("/", headers, async (req, res) => {
+    const pageDetails = {
+      current: "Contact",
+      title: "FMG Furniture | Contact",
+      header: "Contact Us"
+    };
+    const categories = await Category.find({}, { _id: 0 });
     res.render("contact/contact", {
       nav,
       pageDetails,
-      contacts: res.locals.contacts
+      contacts: res.locals.contacts,
+      categories
     });
   });
 
@@ -28,9 +32,7 @@ function contactRouter(nav) {
     const { error } = validate(req.body);
     if (error) {
       pageDetails.error = error.details[0].message;
-      return res
-        .status(400)
-        .render("contact/create", { nav, pageDetails, contactDetails });
+      return res.status(400).redirect("contact/create");
     }
 
     //check that it's not in the db
@@ -43,9 +45,7 @@ function contactRouter(nav) {
     const email = await Contact.findOne({ email: req.body.email });
     if (contact || email || phone) {
       pageDetails.error = "contact already created";
-      return res
-        .status(400)
-        .render("contact/create", { nav, pageDetails, contactDetails });
+      return res.status(400).redirect("contact/create");
     }
 
     // create and save the contact
@@ -57,18 +57,78 @@ function contactRouter(nav) {
 
     try {
       await newContact.save();
-      return res
-        .status(200)
-        .render("contact/create", { nav, pageDetails, contactDetails });
+      return res.status(200).redirect("contact/create");
     } catch (error) {
       pageDetails.error = error;
-      return res
-        .status(500)
-        .render("contact/create", { nav, pageDetails, contactDetails });
+      return res.status(500).render("contact/create");
     }
   });
 
-  return [contactIndex, contactForm, createContact];
+  const viewContacts = router.get("/view", auth, async (req, res) => {
+    const contacts = await Contact.find({}, { _id: 0 });
+    res.render("contact/view", {
+      contacts
+    });
+  });
+
+  const customerMail = router.post("/mail", async (req, res) => {
+    const { error } = validateMail(req.body);
+    if (error) {
+      msg = error;
+      console.log(error);
+      return res.redirect("/");
+    }
+
+    let transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      auth: {
+        user: "veda1@ethereal.email",
+        pass: "nYwckung5GFfQxh7Du"
+      }
+    });
+
+    const mailOptions = {
+      from: "aondonguemma@gmail.com",
+      to: "withtvpeter@gmail.com",
+      subject: `Mail from Customer: ${req.body.name}`,
+      text: `Product Category: ${req.body.category}, 
+        Customer Name: ${req.body.name},
+        Customer Email: ${req.body.email},
+        Customer Phone: ${req.body.phone},
+        Message: ${req.body.message}`
+    };
+
+    try {
+      await transporter.sendMail(mailOptions);
+    } catch (error) {
+      console.log(error);
+    }
+    pageDetails.error = "You mail has been received. Thank you.";
+    return res.redirect("/");
+  });
+
+  return [contactIndex, contactForm, createContact, viewContacts, customerMail];
+}
+
+function validateMail(mail) {
+  const schema = {
+    category: Joi.string().required(),
+    name: Joi.string()
+      .min(4)
+      .max(32)
+      .required(),
+    email: Joi.string()
+      .email({ minDomainAtoms: 2 })
+      .required(),
+    phone: Joi.string()
+      .trim()
+      .min(11)
+      .max(13)
+      .required(),
+    message: Joi.string().required()
+  };
+  return Joi.validate(mail, schema);
 }
 
 module.exports = contactRouter;
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { Service } = require("../models/services");
 const { Quality } = require("../models/about");
 const headers = require("../../middlewares/headers");
+const { Category } = require("../models/category");
 
 pageDetails = {
   current: "Home",
@@ -10,14 +11,16 @@ pageDetails = {
 };
 module.exports = nav => {
   return router.get("/", headers, async (req, res) => {
-    const services = await Service.find({}).limit(3);
-    const qualities = await Quality.find({});
+    const services = await Service.find({}, { _id: 0 }).limit(3);
+    const qualities = await Quality.find({}, { _id: 0 });
+    const categories = await Category.find({}, { _id: 0 });
     res.status(200).render("index", {
       nav,
       pageDetails,
       services,
       contacts: res.locals.contacts,
-      qualities
+      qualities,
+      categories
     });
   });
 };
